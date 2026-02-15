9bf8384dedbec5dc4443dc2181aec43d7869f69f
Tyler Long <aethdae@gmail.com>
2026-02-15 15:17:27 -0600

    comment code to grasp concepts

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const doc = document;
 const main = doc.getElementById("mainContent");
 
 function loadPage(pageToLoad) {
-  const myRequest = new Request(pageToLoad);
-  fetch(myRequest)
+  //Create a request to get a promise
+  fetch(pageToLoad)
+    //Ensure the promise returns ok before getting text
     .then((response) => {
       console.log(response);
       if (!response.ok) {
@@ -11,6 +12,7 @@ function loadPage(pageToLoad) {
       }
       return response.text();
     })
+    //Use the text from ^ response as the page html
     .then((text) => {
       main.innerHTML = text;
     });
